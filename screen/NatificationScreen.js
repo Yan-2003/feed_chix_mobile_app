5e4fc8e898d8170f6f4d66cab084867c778089f3
Julliane Tampus <julliane.tampus123@gmail.com>
2025-01-30 13:01:08 +0800

    load notification to scree

--- screen/NatificationScreen.js
+++ screen/NatificationScreen.js
@@ -3,25 +3,26 @@ import React, { useEffect, useState } from 'react'
 import styles from '../styles/styles'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import axios from 'axios'
+import Loading from '../components/Loading'
 
 export default function NatificationScreen({navigation}) {
 
   const [notification, setnotification] = useState(null);
 
+  const [IsLoading, setIsLoading] = useState(false);
+
 
 
   useEffect( async () => {
 
+    setIsLoading(true)
+
     const response = await axios.get(API_URL + '/notification_log')
 
-    setnotification(response.data.toString())
+    setnotification(response.data)
+    setIsLoading(false)
     
-    
-
   }, []);
-
-
-
 
   return (
     <SafeAreaView style={styles.safeArea}>
@@ -33,11 +34,18 @@ export default function NatificationScreen({navigation}) {
        </View>
       <ScrollView style={styles.notification_box}>
 
-        <View style={styles.notification_message}>
-        <Text style={styles.notification_massage_time}>10:30 am</Text>
-          <Text style={styles.notification_massage_text}>Hello World</Text>
-          <Text>{notification}</Text>
-        </View>
+        {
+          IsLoading ? <Loading/>
+
+          : notification.forEach(notif => {
+              <View style={styles.notification_message}>
+              <Text style={styles.notification_massage_time}>10:30 am</Text>
+                <Text style={styles.notification_massage_text}>{notif.message}</Text>
+              </View>
+            })
+
+        }
+
       </ScrollView> 
     </SafeAreaView>
   )
